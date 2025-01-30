ace841dd2069971d379458858f49f60092fc6e63
Wojciech Mietlinski <votech@otovo.com>
2025-01-30 13:08:04 +0100

    Add blog headline

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import ContentWrapper from '@/components/ContentWrapper/ContentWrapper';
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <ContentWrapper>
+        <h1 className="py-5 text-2xl md:py-10 md:text-3xl">Blog Edukacyjny</h1>
+      </ContentWrapper>
+    </div>
+  );
 }
