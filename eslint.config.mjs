81edb0787f4f2f305ab955952d4bd6c352c0dab4
Wojciech Mietlinski <votech@otovo.com>
2025-01-30 11:00:28 +0100

    ESLint: Force newline after imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import { FlatCompat } from '@eslint/eslintrc';
-import eslintConfigPrettier from 'eslint-config-prettier';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -11,8 +10,26 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  eslintConfigPrettier,
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'next/typescript',
+      'prettier',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+    ],
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      'import/newline-after-import': ['error', { count: 1 }],
+    },
+  }),
 ];
 
 export default eslintConfig;
